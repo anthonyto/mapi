bcffc621c568cb76eac3ace380dedef8cf780717
Anthony To <to.anthony93@gmail.com>
2014-02-01 07:13:29 -0600

    Fix rails CORS

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,10 +1,12 @@
 class MealsController < ApplicationController
+  before_filter :set_headers
+
 
   # GET /meals
   # GET /index.rabl
   def index
     @meals = Meal.all
-    render json: @meals
+    # render json: @meals
   end
   
   # GET /meals/1
@@ -41,6 +43,14 @@ class MealsController < ApplicationController
   end
   
   private 
+  
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
+  end
   
   def meal_params
     params.require(:meal).permit(:name, :type, :email, :lat, :long, :address, :description)
