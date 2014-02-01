f7eef105f868044bf42ce9efff472f2c343578fe
Anthony To <to.anthony93@gmail.com>
2014-01-31 22:27:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mealr::Application.config.secret_key_base = '51f71330350cdd2cad5355e49365dcddd604f33b20bb97757829836fac5328c71f25f004a9b941ab3b661d989374c7b645f2b65a9dba411b3efbd15bbd062db0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mealr::Application.config.session_store :cookie_store, key: '_mealr_session'
