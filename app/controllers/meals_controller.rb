a8accba5a8c5fda364d5e790b0ae808a53dbb8da
Anthony To <to.anthony93@gmail.com>
2014-02-01 12:38:59 -0600

    Remove cors from controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -43,14 +43,7 @@ class MealsController < ApplicationController
   end
   
   private 
-  
-  def set_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
-    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, HEAD'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    headers['Access-Control-Max-Age'] = '86400'
-  end
+
   
   def meal_params
     # params.require(:meal).permit(:name, :type, :email, :lat, :long, :address, :description, :plates)
